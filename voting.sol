d3bc4d89c2f4e4ce4590a09afc2b72d4aa575033
Lin-Yi Tang <lursun914013@gmail.com>
2018-10-18 16:11:12 +0800

    update

--- voting.sol
+++ voting.sol
@@ -1,37 +1,36 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.23;
+pragma experimental ABIEncoderV2;
 
 contract Voting {
-
   mapping (bytes32 => uint8) public votesReceived;
 
-  // 存储候选人名字的数组
-  bytes32[] public candidateList;
+  
+  string[] public candidateList;
 
 
   // 构造函数 初始化候选人名单
-  function Voting(bytes32[] candidateNames) {
-
+  constructor(string[] candidateNames) public {
     candidateList = candidateNames;
   }
 
   // 查询某个候选人的总票数
-  function totalVotesFor(bytes32 candidate)  constant returns (uint8) {
+  function totalVotesFor(string candidate)  constant public returns (uint8) {
     require(validCandidate(candidate) == true);
     // 或者
     // assert(validCandidate(candidate) == true);
-    return votesReceived[candidate];
+    return votesReceived[keccak256(candidate)];
   }
 
   // 为某个候选人投票
-  function voteForCandidate(bytes32 candidate) {
+  function voteForCandidate(string candidate) public {
     assert(validCandidate(candidate) == true);
-    votesReceived[candidate] += 1;
+    votesReceived[keccak256(candidate)] += 1;
   }
 
   // 检索投票的姓名是不是候选人的名字
-  function validCandidate(bytes32 candidate) constant returns (bool) {
+  function validCandidate(string candidate) constant public returns (bool) {
     for(uint i = 0; i < candidateList.length; i++) {
-      if (candidateList[i] == candidate) {
+      if (keccak256(candidateList[i]) == keccak256(candidate)) {
         return true;
       }
     }
